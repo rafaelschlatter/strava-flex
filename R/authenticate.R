87762cb9c850a58f3a333fd5e62f6ae8a47d1fc8
Rafael <rafaelschlatter@gmail.com>
2019-07-23 17:33:32 +0200

    FINALLY managed to get this on shinyapps.io, biggest hack ever.

--- R/authenticate.R
+++ R/authenticate.R
@@ -7,6 +7,14 @@ stoken <- httr::config(
     strava_keys$app_name,
     strava_keys$client_id,
     strava_keys$client_secret,
-    app_scope = 'view_private'
+    app_scope = 'view_private',
+    cache = TRUE
   )
 )
+
+# This line needs to be disabled when running httr::config() for the first time.
+# A token will be cached in the working directory. This token can be used on
+# the shiny server, where manuel accepting the request through Stravas website
+# is not possible. After running above code once locally, upload the whole directory
+# to shinyapps.io (super hacky, but worked for me at least).
+stoken <- httr::config(token = readRDS('.httr-oauth')[[1]])
